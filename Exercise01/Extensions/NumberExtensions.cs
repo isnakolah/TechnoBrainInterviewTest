6561af2c48241054b6b123838ef3003ce4043ffe
Daniel Nakolah <nakolahdaniel1@gmail.com>
2022-03-02 07:52:39 +0300

    Create extension methods for int and BigInteger

--- Exercise01/Extensions/NumberExtensions.cs
+++ Exercise01/Extensions/NumberExtensions.cs
@@ -1,16 +1,11 @@
 using System.Numerics;
+using Exercise01.Services;
 
 namespace Exercise01.Extensions;
 
 public static class NumberExtensions
 {
-    public static string Towards(this int num)
-    {
-        return num.ToString();
-    }
+    public static string Towards(this int num) => TowardsService.Towards(num);
 
-    public static string Towards(this BigInteger num)
-    {
-        return num.ToString();
-    }
+    public static string Towards(this BigInteger num) => TowardsService.Towards(num);
 }
